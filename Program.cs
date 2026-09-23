7bf602db6bb1474d4fa11017d6c635f8f95abd9f
Mauricio <mauvillaarias10@gmail.com>
2026-09-22 19:13:11 -0500

    REQ-01: estructurar variables para registro basico de solicitud

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string codigo = "";
+        string nombre = "";
+        string tipo = "";
+        string desc = "";
+    }
+}
